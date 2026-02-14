ee8ed3e3ab836d6181c27493a06ac09c435d6d64
GorexTest <googlingrex@gmail.com>
2026-02-14 21:24:19 +0700

    New Workflows

--- profiler/jni/Android.mk
+++ profiler/jni/Android.mk
@@ -1,14 +1,31 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := nusantara_profiler
 LOCAL_SRC_FILES := ../src/nusantara_profiler.c
 
-LOCAL_CFLAGS := -DNDEBUG -Wall -Wextra -Werror \
-                -pedantic-errors -Wpedantic \
-                -O2 -std=c23 -fPIC -flto
+LOCAL_CFLAGS := \
+    -DNDEBUG \
+    -Wall \
+    -Wextra \
+    -Wno-unused-parameter \
+    -Wno-unused-variable \
+    -pedantic-errors \
+    -Wpedantic \
+    -O2 \
+    -std=gnu17 \
+    -fPIC \
+    -D_GNU_SOURCE \
+    -ffunction-sections \
+    -fdata-sections
 
-LOCAL_LDFLAGS := -flto
-LOCAL_LDLIBS  += -llog  
+LOCAL_LDFLAGS := \
+    -Wl,--gc-sections \
+    -Wl,-z,relro \
+    -Wl,-z,now
+
+# Android log lib
+LOCAL_LDLIBS += -llog
 
 include $(BUILD_EXECUTABLE)
